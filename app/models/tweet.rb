3e9a9883ad567335a5c8fc53272f906432b9535d
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-19 16:49:34 -0300

    adds tweets validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,7 @@ class Tweet < ApplicationRecord
 
   has_many :replies, class_name: "Tweet", foreign_key: "source_id"
   belongs_to :source, class_name: "Tweet", optional: true
+
+  validates :content, presence: true
+  validates :content, length: { maximum: 140 }
 end
