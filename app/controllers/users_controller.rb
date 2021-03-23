3150f2adde9a695bd8c3b23f4e8f428b3f0e350c
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-23 14:28:59 -0300

    adds following user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %I[show follow]
 
   def show
+  end
+
+  def follow
+    current_user.followings << @user
+    render :show
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
