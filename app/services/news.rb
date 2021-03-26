2a4d1dee33d14e06de9b5d5baa32b172e311563e
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-03-26 17:55:34 -0300

    Puts top news on Home

--- app/services/news.rb
+++ app/services/news.rb
@@ -5,6 +5,7 @@ module News
       url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ENV['API_KEY']}"
       req = open(url)
       response_body = req.read
+      response = JSON.parse(response_body)
     end
   end
 end
